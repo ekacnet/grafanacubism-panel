780314871b0460e6a222e915b34d30434969faf7
Matthieu Patou <mat@matws.net>
2024-02-29 12:19:24 -0800

    Starting the plugin
    It's working to some extent.
    A bit of polish is most probably needed

---
+++ cypress/integration/02-get-graph.ts
@@ -0,0 +1,15 @@
+import { e2e } from '@grafana/e2e';
+const dashboardUID = 'b6610ab3-816b-4649-9909-a6bf32605b8b'
+
+e2e.scenario({
+  describeName: 'Get cubism graph',
+  itName: 'Get cubism test',
+  scenario: () => {
+    e2e.pages.Dashboard.visit(dashboardUID)
+    e2e().contains('Panel Title').should('be.visible');
+    // Check if there are 19 div elements with the class 'horizon'
+    e2e().get('div.horizon').should('have.length', 10)
+    e2e().get('div.horizon').first().next().find('span.title').should('contain', 'A-series1')
+    e2e().get('div.horizon').first().find('canvas').should('be.visible');
+  },
+});
