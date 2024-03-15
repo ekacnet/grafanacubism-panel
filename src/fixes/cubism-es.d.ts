a3b6b3aec6f9ae46b03b6c65a5277a5108c30227
Matthieu Patou <mat@matws.net>
2024-03-15 11:27:29 -0700

    Make the display of message optional
    Also allow control on the extent to either use automatic extents or
    manual ones

--- src/fixes/cubism-es.d.ts
+++ src/fixes/cubism-es.d.ts
@@ -1,14 +1,13 @@
 declare module 'cubism-es' {
-    export interface Context {
-        metric: any;
-        rule: any;
-        horizon: any;
-        on: any;
-        size(_s: number ):  Context;
-        size():  number;
-        axis(): any;
-        step(_s: number): number;
-    }
-    export function context(): Context;
+  export interface Context {
+    metric: any;
+    rule: any;
+    horizon: any;
+    on: any;
+    size(_s: number): Context;
+    size(): number;
+    axis(): any;
+    step(_s: number): number;
+  }
+  export function context(): Context;
 }
-
